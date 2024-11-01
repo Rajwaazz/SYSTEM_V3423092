5bf97c27d8405f6177a12cde2caf2045f391a594
Rajwaazz <muhammadrajwa124@gmail.com>
2024-11-02 00:51:39 +0700

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HeaderContent from '@/components/NavbarContent.vue'
+import FooterContent from '@/components/FooterContent.vue'
+</script>
+
+<template>
+  <HeaderContent />
+  <main>
+    <slot></slot>
+  </main>
+  <FooterContent />
+</template>
